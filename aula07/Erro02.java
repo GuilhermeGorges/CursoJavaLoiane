e27ddbd71926e2402e64756971e2d68bd0e0a543
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-05-20 11:58:59 -0300

    Second Commit :rocket:

---
+++ aula07/Erro02.java
@@ -0,0 +1,10 @@
+// erro de semântica 
+
+class Erro02 {
+	
+	public static void main (String[] args){
+		var int cont; // int cont = 0 
+
+		System.out.println("Você digitou: " + cont);
+	}
+}
---
+++ aula07/Erro03.java
@@ -0,0 +1,16 @@
+// erros que só acontecem em tempo de execução
+class Erro03 {
+	
+	public static void main (String[] args){
+
+		System.out.println("Você digitou: " + 1/0);
+	}
+}
+
+class Erro03 {
+	
+	public static void Main (String[] args){
+
+		System.out.println("Você digitou: " );
+	}
+}
