7d1a741283ffd970c0a9909b350fa9565d038052
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-05-21 09:48:22 -0300

    fourth Commit :rocket:

---
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula10/Variaveis.java
@@ -0,0 +1,14 @@
+package com.guilherme.cursojava.aula10;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        
+        int age = 29;
+        String name = "Guilherme";
+        String nameOfMyCat = "Phoebe";
+        
+        System.out.println("Nome = " + name + " Idade= " + age + " Name of my cat= " + nameOfMyCat);
+        
+        
+    }
+}
