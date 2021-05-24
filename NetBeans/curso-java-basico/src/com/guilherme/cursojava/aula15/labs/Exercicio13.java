45b5821b665dc1406c46e1efd973788954ed5dea
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-05-24 13:45:16 -0300

    sixth Commit :rocket:

--- NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula15/labs/Exercicio13.java
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula15/labs/Exercicio13.java
@@ -6,11 +6,30 @@
 package com.guilherme.cursojava.aula15.labs;
 
 import com.guilherme.cursojava.aula13.labs.*;
+import java.util.Scanner;
 
 /**
  *
  * @author Gui e Di
  */
 public class Exercicio13 {
-    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        while(true){
+        System.out.println("Digite o numero do dia da semana: ");
+        int diaSem = scan.nextInt();
+        switch (diaSem){
+            case 1: System.out.println("O dia da semana é Domingo");break;
+            case 2: System.out.println("O dia da semana é Segudna-Feira");break;
+            case 3: System.out.println("O dia da semana é Terça-Feira");break;
+            case 4: System.out.println("O dia da semana é Quarta-Feira");break;
+            case 5: System.out.println("O dia da semana é Quinta-Feira");break;
+            case 6: System.out.println("O dia da semana é Sexta-Feira");break;
+            case 7: System.out.println("O dia da semana é Sábado");break;
+            default :System.out.println("Dia da semana inexistente");
+        }
+        }
+    }
 }
