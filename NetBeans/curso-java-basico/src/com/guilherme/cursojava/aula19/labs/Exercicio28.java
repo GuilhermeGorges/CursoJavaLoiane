3d07b51025a8b290e4e8e87b602bc38aa012cfe6
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-05-29 13:33:00 -0300

    update exercicio de arrays

--- NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula19/labs/Exercicio28.java
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula19/labs/Exercicio28.java
@@ -14,5 +14,29 @@ import com.guilherme.cursojava.aula13.labs.*;
  * @author Gui e Di
  */
 public class Exercicio28 {
-    
+    public static void main(String[] args) {
+        java.util.Scanner scan = new java.util.Scanner(System.in);
+        
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[vetorA.length];
+        
+        for (int i = 0; i < vetorA.length; i++){
+            System.out.println("Digite o valor da posição " + i);
+            vetorA[i] = scan.nextInt();       
+        }
+        
+        for (int i = 0, j = (vetorA.length-1); i < vetorA.length; i++, j--){
+            vetorB[i] = vetorA[j];
+        }
+        
+        System.out.println("Vetor A = ");
+        for (int i = 0; i < vetorA.length; i++){
+            System.out.print(vetorA[i]+" ");
+        }
+        System.out.println();
+        System.out.println("Vetor B = ");
+        for (int i = 0; i < vetorB.length; i++){
+            System.out.print(vetorB[i]+" ");
+        }
+    }
 }
