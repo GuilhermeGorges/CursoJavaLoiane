6dba058b6d584cd8c2943268361ecebcaf822db5
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-06-02 18:17:51 -0300

    Orientacao a objetos - Aula this 30 .Commit

---
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula30this/Carro.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guilherme.cursojava.aula30this;
+
+/**
+ *
+ * @author Gui e Di
+ */
+public class Carro {
+    // atributos  
+
+    String marca;
+    String modelo;
+    int numPassageiros;
+    double capCombustivel;
+    double consumoCombustivel;
+
+
+    public Carro(String marca, String modelo, int numPassageiros, double capCombustivel, double consumoCombustivel) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.numPassageiros = numPassageiros;
+        this.capCombustivel = capCombustivel;
+        this.consumoCombustivel = consumoCombustivel;
+    }
+    
+    public Carro() {}
+
+    public Carro(String marca, String modelo, int numPassageiros) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.numPassageiros = numPassageiros;
+        System.out.println("Chamando construtor com 3 parametros.");
+    }
+    
+    
+    public Carro(String marca, String modelo) {
+        this(marca, modelo, 10);
+        System.out.println("Chamando construtor com 2 parametros.");
+        
+    }
+    
+    
+    
+    
+    // metodos 
+
+    
+    void exibirAutonomia() {
+        System.out.println("A autonomia do carro é: " + this.capCombustivel * this.consumoCombustivel + " km");
+    }
+
+    double obterAutonomia() {
+
+        System.out.println("Método obterAutonomia foi chamado.");
+
+        return this.capCombustivel * this.consumoCombustivel;
+    }
+
+    double calcularCombustivel(double km) {
+
+        double qtdCombustivel = km / this.consumoCombustivel;
+
+        return qtdCombustivel;
+    }
+
+}
---
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula30this/TesteCarro.java
@@ -0,0 +1,10 @@
+package com.guilherme.cursojava.aula30this;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+        
+        Carro van = new Carro("Fiat", "Ducato");
+        
+        System.out.println(van.numPassageiros);
+    }
+}
