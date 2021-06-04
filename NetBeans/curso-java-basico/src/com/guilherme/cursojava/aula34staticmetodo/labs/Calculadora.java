2f66741b65b83bbf24153153c0f51e7e3b4d5eae
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-06-04 15:36:17 -0300

    Orientacao a objetos - Aula 34 aplicabilidade do metodo static Exercicios .Commit

--- NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula34staticmetodo/labs/Calculadora.java
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula34staticmetodo/labs/Calculadora.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.guilherme.cursojava.aula34staticmetodo.labs;
 
-/**
- *
- * @author Gui e Di
- */
 public class Calculadora {
     public static double somar(double n1, double n2){
         return  n1 + n2;
