1a2d8c2ccf6bf71dcbed8211c514ebd3a996e24e
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-06-07 11:46:44 -0300

    Orientacao a objetos - Exercicios 36 Relacionamento entre classes .Commit

---
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula36relacionamentoentreclasses/labs/Agenda.java
@@ -0,0 +1,58 @@
+
+package com.guilherme.cursojava.aula36relacionamentoentreclasses.labs;
+
+public class Agenda {
+    
+    private String nome;
+    private Contato[] contatos;
+
+    
+    
+    public Agenda(){        
+    }
+    
+    
+    public Agenda(String nome){  
+        this.nome = nome;
+    }
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the contatos
+     */
+    public Contato[] getContatos() {
+        return contatos;
+    }
+
+    /**
+     * @param contatos the contatos to set
+     */
+    public void setContatos(Contato[] contatos) {
+        this.contatos = contatos;
+    }
+    
+    public String obterInfo(){
+        String info = "Nome = " + nome + "\n";
+        
+        if (contatos != null){
+            for (Contato c : contatos){
+                info += c.obterInfo() + "\n";
+            }
+        }
+        
+        return info;
+    }
+    
+}
