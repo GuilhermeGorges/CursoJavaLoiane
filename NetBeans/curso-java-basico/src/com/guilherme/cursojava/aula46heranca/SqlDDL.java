d96e1ad254231243aaaa1294910b3d9fa9597467
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-06-09 14:33:05 -0300

    Orientacao a objetos - Aulas 46 heranca de interfaces.Commit

---
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula46heranca/SqlDDL.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guilherme.cursojava.aula46heranca;
+
+/**
+ *
+ * @author Gui e Di
+ */
+public interface SqlDDL{
+    void create(String query);
+    void drop(String query);
+    void alter(String query);
+    
+}
