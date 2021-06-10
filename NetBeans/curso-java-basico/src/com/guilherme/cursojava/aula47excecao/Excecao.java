313b5d0b0fcccd4415633d6d4e57bd58dbfcbcb5
Guilhermegs <guilhermegabriel1992@gmail.com>
2021-06-10 15:47:27 -0300

    Orientacao a objetos - Aula tratamento Excecao 47, 48, 49.Commit

---
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula47excecao/Excecao.java
@@ -0,0 +1,24 @@
+
+package com.guilherme.cursojava.aula47excecao;
+
+public class Excecao {
+    public static void main(String[] args) {
+        int[] vetor = new int[4];        
+      
+        try {
+            System.out.println("Será impresso");
+        
+            vetor[4] = 1;
+            System.out.println("Não será");
+        }
+        catch (ArrayIndexOutOfBoundsException exception){
+            System.out.println("Acessar indice do vetor inexistente");
+        }
+        
+        System.out.println("Impresso apos a exeption ");
+        
+        
+        
+    }
+}
+ 
