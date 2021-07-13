4099f325697ecdb0457a50ea5bb201a122d9a713
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-07-13 12:32:36 -0300

    Aula 54

---
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula54/enumeradores/TesteEnum.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guilherme.cursojava.aula54.enumeradores;
+
+/**
+ *
+ * @author Gui e Di
+ */
+public class TesteEnum {
+    public static void main(String[] args) {
+        
+        DiaSemana dia = DiaSemana.SEGUNDA;
+        
+        
+        System.out.println(dia.toString() + " - " + dia.getValor());
+        
+        Data data = new Data(1, 4, 2000, DiaSemana.SEXTA);
+    }
+}
