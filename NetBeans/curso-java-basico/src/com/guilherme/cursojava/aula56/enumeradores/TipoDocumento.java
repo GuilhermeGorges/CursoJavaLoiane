14b2e305a1240710c8cdbd606926c81817cc09a3
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-07-13 13:03:49 -0300

    Aula enumeradores 56

---
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula56/enumeradores/TipoDocumento.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guilherme.cursojava.aula56.enumeradores;
+
+/**
+ *
+ * @author Gui e Di
+ */
+public enum TipoDocumento {
+    
+    CPF {
+        @Override
+        public String geraNumeroTeste() {
+            return GeraCpfCnpj.cpf();
+        }
+    }, CNPJ {
+        @Override
+        public String geraNumeroTeste() {
+            return GeraCpfCnpj.cnpj();
+        }
+    };
+    
+    public abstract String geraNumeroTeste();
+    
+}
