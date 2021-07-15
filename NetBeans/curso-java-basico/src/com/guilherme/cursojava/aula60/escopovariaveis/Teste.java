a7d809a65e9f9c188872b875a58366d92bc79eeb
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-07-15 11:53:21 -0300

    Aula 60 + 61

---
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula60/escopovariaveis/Teste.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.guilherme.cursojava.aula60.escopovariaveis;
+
+/**
+ *
+ * @author Gui e Di
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+
+        EscopoVariaveis escopo = new  EscopoVariaveis();
+        escopo.setValor(10);
+        
+        System.out.println(escopo.getValor());
+        
+        System.out.println(escopo.calculaValor(20));
+        
+        System.out.println(escopo.getValor());
+        
+        System.out.println(escopo.teste());
+        
+        System.out.println(escopo.getValor());
+    }
+}
