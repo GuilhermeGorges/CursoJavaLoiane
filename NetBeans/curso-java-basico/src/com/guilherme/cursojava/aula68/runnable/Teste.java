e220fc61d2baf0405e00e66a7e27fb2b07ddf8d0
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-07-17 20:03:45 -0300

    Aula 69

--- NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula68/runnable/Teste.java
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula68/runnable/Teste.java
@@ -4,13 +4,14 @@ public class Teste {
 
     public static void main(String[] args) {
 
-        MinhaThreadRunnable thread1 = new MinhaThreadRunnable("#1", 900);
+        MinhaThreadRunnable thread1 = new MinhaThreadRunnable("#1", 888);
         //Thread t1 = new Thread(thread1);
         //t1.start();
 
-        MinhaThreadRunnable thread2 = new MinhaThreadRunnable("#2", 650);
+        MinhaThreadRunnable thread2 = new MinhaThreadRunnable("#2", 555);
 
-        MinhaThreadRunnable thread3 = new MinhaThreadRunnable("#3", 1100);
+        MinhaThreadRunnable thread3 = new MinhaThreadRunnable("#3", 1111);
     }
 
 }
+ 
