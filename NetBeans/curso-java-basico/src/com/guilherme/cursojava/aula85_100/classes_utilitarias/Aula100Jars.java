1e9988c68483bd4ab2949744e1a45966637ed522
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-07-24 19:09:41 -0300

    Aulas 93 - 100 classes utilitarias

---
+++ NetBeans/curso-java-basico/src/com/guilherme/cursojava/aula85_100/classes_utilitarias/Aula100Jars.java
@@ -0,0 +1,13 @@
+package com.guilherme.cursojava.aula85_100.classes_utilitarias;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+
+public class Aula100Jars {
+
+    public static void main(String[] args) {
+
+        System.out.println(StringUtils.capitalize("Guilherme"));
+
+    }
+
+}
